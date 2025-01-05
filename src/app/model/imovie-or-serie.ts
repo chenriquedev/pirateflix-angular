db4a6f7008571759a55c0b3d26800e8eda3348a5
Carlos Henrique <110176211+chenriquedev@users.noreply.github.com>
2025-01-05 12:33:28 -0300

    Setup models
    - Inserted movie-or-serie model
    - Inserted movie model
    - Inserted serie model
    - Inserted response model

--- src/app/model/imovie-or-serie.ts
+++ src/app/model/imovie-or-serie.ts
@@ -1,2 +1,11 @@
 export interface IMovieOrSerie {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  title: number;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  release_date: string;
+  vote_average: number
 }
