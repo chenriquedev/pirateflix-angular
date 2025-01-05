8cf5825c9d4c11c3889a36a94263ceed6c90c439
chenriquedev <krlos.henrique98@gmail.com>
2025-01-05 12:31:20 -0300

    Setup models - Inserted movie-or-serie model - Inserted movie model - Inserted serie model - Inserted response model

--- src/app/model/imovie.ts
+++ src/app/model/imovie.ts
@@ -1,2 +1,12 @@
 export interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  runtime: number;
+  tagline: string;
+  title: string;
+  vote_average: number
 }
--- src/app/model/iserie.ts
+++ src/app/model/iserie.ts
@@ -1,2 +1,12 @@
 export interface ISerie {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  last_air_date: string;
+  name: string;
+  overview: string;
+  tagline: string;
+  poster_path: string;
+  status: string;
+  vote_average: number;
 }
