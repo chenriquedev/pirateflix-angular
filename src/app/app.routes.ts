73648bf8228c38622ccea41146105fb010d02cd0
chenriquedev <krlos.henrique98@gmail.com>
2025-01-07 16:44:26 -0300

    Created movie and series page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
+import { MoviesComponent } from './page/movies/movies.component';
+import { SeriesComponent } from './page/series/series.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
     title: "HOME"
+  },
+  {
+    path: "movies",
+    component: MoviesComponent,
+    title: "MOVIES"
+  },
+  {
+    path: "series",
+    component: SeriesComponent,
+    title: "SERIES"
   }
 ];
