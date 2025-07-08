59ee28748f1fc1fa1eb6eea57e0abfef3f6597f3
Carlos Henrique Pinto <110176211+chenriquedev@users.noreply.github.com>
2025-07-08 11:30:25 -0300

    remove console.log and change method on localstorage favorite item (#1) (#3)

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,16 +1,28 @@
 import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { catchError, of, tap } from 'rxjs';
+import { ToastService } from '../service/toast.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const toastService = inject(ToastService);
   const reqWithHeader = req.clone({
-    headers: req.headers.set('Authorization', `Bearer ${import.meta.env.NG_APP_TOKEN}`),
+    headers: req.headers.set(
+      'Authorization',
+      `Bearer ${import.meta.env.NG_APP_TOKEN}`
+    ),
   });
 
   return next(reqWithHeader).pipe(
     tap((event) => {
-      return event
+      return event;
     }),
     catchError((error: HttpResponse<unknown>) => {
+      toastService.showToast(
+        'error',
+        'Erro',
+        error.statusText || 'Ocorreu um erro'
+      );
+      console.log(error);
       return of(error);
     })
   );
